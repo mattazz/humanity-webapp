d0504a4b37df9a92ec1c092eaf4295978c055fd5
Mattazz <matthewazada101@gmail.com>
2022-12-20 18:38:51 +0800

    Adding to database

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 import { db_FirstNames, db_LastNames, 
     db_positiveAttr, db_negativeAttr,
-db_milestone_events} from './db.js'
+db_milestone_general_events} from './db.js'
 
 let firstNames = db_FirstNames
 let lastNames = db_LastNames
 let positiveAttr = db_positiveAttr
 let negativeAttr = db_negativeAttr
-let milestoneEvents = db_milestone_events
+let milestoneGenEvents = db_milestone_general_events
 
 // Returns list [0] = First name [1] = Last name
 function generate_name(){
@@ -25,7 +25,7 @@ function generate_attributes(max_amount){
         let totalAttr = [positiveAttr, negativeAttr]
         let category = totalAttr[generateRandomInt(totalAttr.length)]
         let attr = category[generateRandomInt(category.length)]
-        if (final_list.indexOf(attr) > -1){
+        while (final_list.indexOf(attr) != -1){
             console.log('Found duplicate attribute: ' + attr +  ' | Selecting another...')
             attr = category[generateRandomInt(category.length)]
         }
@@ -35,19 +35,14 @@ function generate_attributes(max_amount){
     return final_list
 }
 
-function generate_milestones(age, max_milestones){
-    let era = 'toddler'
-    let selectedEra = milestoneEvents[era]
-    let event = selectedEra[generateRandomInt(selectedEra.length)]
-    
-    console.log(event)
-}
-
-
 // For generating random numbers
 function generateRandomInt(max){
     let randNum = Math.floor(Math.random() * max)
     return randNum
+}
+
+function between(num, min, max){
+    return num >= min && num <= max
 }
 
 // Main character class
@@ -55,7 +50,7 @@ class character{
     constructor({fName = generate_name()[0], 
         lName = generate_name()[1], 
         age = 0, 
-        attr = generate_attributes(3),
+        attr = generate_attributes(5),
         events = []
     }){
         this.fName = fName
@@ -65,9 +60,67 @@ class character{
         this.attr = attr
         this.events = events
     }
+    // Used to generate events in their lives
+    generate_milestones(age, max_milestones){
+        let era = ''
+        if(between(this.age,0, 4)){
+            era = 'infant'
+        } else if(between(this.age, 5, 11)) {
+            era = 'toddler'
+        } else if(between(this.age, 12, 17)){
+            era = 'child'
+        } else if(between(this.age, 18, 24)){
+            era = 'young_adult'
+        } else if(between(this.age, 25, 999)){
+            era = 'adult'
+        }
+
+        let positivity = ['positive', 'negative']
+        let type = ['physical', 'mental', 'social']
+
+        console.log('Generating milestone for Era: ' + era)
+
+        let i = 0
+        while(i < max_milestones){
+            let selectedEra = milestoneGenEvents[era]
+            let selectedPositivity = positivity[generateRandomInt(positivity.length)]
+            let selectedType = type[generateRandomInt(type.length)]
+            console.log(`Generating... || ${era} || ${selectedPositivity} || ${selectedType}`)
+            let event = selectedEra[selectedPositivity][selectedType][0]['event']
+            console.log("Specific event: " + event)
+            console.log('Generating event for: ' + this.fName + ' : ' + event)
+            this.events.push(event)
+            i++
+
+            // Writing to HTML
+            $('#events').before(event);
+        }   
+    }
+
+    age_up(number_of_years){
+        this.age += number_of_years
+    }
+
+    age_up_w_milestones(number_of_years){
+        let i = 0
+        while(i<number_of_years){
+            this.age ++
+            i ++
+            console.log('Aged up! New age is: ' + this.age)
+            
+            // Writing to HTML test
+            $('#events').before(`<h5> Age: ${this.age}`);
+
+            this.generate_milestones(this.age, 1) //Sets number of milestones per year
+
+        }
+    }
 }
 
 // Testing character generation
 let char1 = new character({})
-
-generate_milestones(0,1)
+$('h1').text(char1.fName + ' ' + char1.lName);
+$('h1').after(`<p>Attributes: ${char1.attr}`);
+char1.age_up_w_milestones(25)
+$('h1').after(`<p>Age:  ${char1.age} </p>`);
+console.log(char1)
--- db.js
+++ db.js
@@ -1,13 +1,273 @@
 export let db_FirstNames = ['Matt', 'Adi', 'Shadow']
 export let db_LastNames = ['Azada', 'Amor', 'Shiba']
 
-export let db_positiveAttr = ['Brave', 'Intelligent', 'Charismatic']
-export let db_negativeAttr = ['Clumsy', 'Ugly']
+export let db_positiveAttr = ['Brave', 'Intelligent', 'Charismatic', 'Attractive', 'Social']
+export let db_negativeAttr = ['Clumsy', 'Ugly', 'Fickle', 'Evil', 'Introvert']
 
-export let db_milestone_events = {
-    infant: ['Fell', 'Started walking'],
-    toddler: ['Learned how to speak', 'Got reprimanded'],
-    child: ['Excelled at school', 'Got bullied at school'],
-    young_adult: ['Got A+', 'Stole from someone'],
-    adult: ['Got fired', 'Got a promotion']
+
+// Replace above if it works
+// Severity = scale from -10 to 10 neg to pos
+// Category = physical, mental, or social
+export let db_milestone_general_events = {
+    infant: {
+        positive:{
+            physical:
+                [
+                {event: 'Ate a good meal',
+                severity: 0,
+                condition: None},
+                {event: 'positive infant event',
+                severity: 0}
+            ],
+            mental:
+                [
+                {event: 'Listened to good music',
+                severity: 0,
+                condition: None}
+            ],
+            social:
+                [
+                {event: 'Played with ma-ma',
+                severity: 0,
+                condition: None}
+            ],
+        },
+        negative:{
+            physical:
+                [
+                {event: 'Ate something spoiled',
+                severity: 0,
+                condition: None},
+                {event: 'Rolled around my bend',
+                severity: 0,
+                condition: None}
+            ],
+            mental:
+                [
+                {event: 'Everyone was too loud!',
+                severity: 0,
+                condition: None}
+            ],
+            social:
+                [
+                {event: 'Ma-ma got mad at me',
+                severity: 0,
+                condition: None}
+            ],
+        }
+    },
+    toddler: {
+        positive:{
+            physical:
+                [
+                {event: 'Visited a park',
+                severity: 0},
+                {event: 'Met a dog today',
+                severity: 0,
+                condition: None}
+            ],
+            mental:
+                [
+                {event: 'Remembered a nice memory',
+                severity: 0,
+                condition: None}
+            ],
+            social:
+                [
+                {event: 'Had a play date with a friend',
+                severity: 0,
+                condition: None}
+            ],
+        },
+        negative:{
+            physical:
+                [
+                {event: 'Bit my tongue!',
+                severity: 0,
+                condition: None},
+                {event: 'Smelled a fart!',
+                severity: 0,
+                condition: None}
+            ],
+            mental:
+                [
+                {event: 'Got a headache',
+                severity: 0,
+                condition: None}
+            ],
+            social:
+                [
+                {event: 'A kid pushed me!',
+                severity: 0,
+                condition: None}
+            ],
+        }
+    },
+    child: {
+        positive:{
+            physical:
+                [
+                {event: 'Ate a good meal',
+                severity: 0,
+                condition: None},
+                {event: 'Friend told me I was good looking',
+                severity: 0,
+                condition: None}
+            ],
+            mental:
+                [
+                {event: 'Solved a puzzle!',
+                severity: 0,
+                condition: None}
+            ],
+            social:
+                [
+                {event: 'Played with friends at the park',
+                severity: 0,
+                condition: None}
+            ],
+        },
+        negative:{
+            physical:
+                [
+                {event: 'Injured my knee',
+                severity: 0,
+                condition: None},
+                {event: 'Had a cough.',
+                severity: 0,
+                condition: None}
+            ],
+            mental:
+                [
+                {event: 'Could not solve a puzzle',
+                severity: 0,
+                condition: None}
+            ],
+            social:
+                [
+                {event: 'I was left alone at home',
+                severity: 0,
+                condition: None}
+            ],
+        }
+    },
+    young_adult: {
+        positive:{
+            physical:
+                [
+                {event: 'Got good grades this sem',
+                severity: 0,
+                condition: None},
+                {event: 'Had a good date',
+                severity: 0,
+                condition: None}
+            ],
+            mental:
+                [
+                {event: 'Listened to good music',
+                severity: 0,
+                condition: None}
+            ],
+            social:
+                [
+                {event: 'Spent time with my family',
+                severity: 0,
+                condition: None}
+            ],
+        },
+        negative:{
+            physical:
+                [
+                {event: 'Broke my wrist',
+                severity: 0,
+                condition: None},
+                {event: 'Got bullied at school',
+                severity: 0,
+                condition: None}
+            ],
+            mental:
+                [
+                {event: 'Could not finish my homework',
+                severity: 0,
+                condition: None}
+            ],
+            social:
+                [
+                {event: 'Played with ma-ma',
+                severity: 0,
+                condition: None}
+            ],
+        }
+    },
+    adult: {
+        positive:{
+            physical:
+                [
+                {event: 'Ate a good meal',
+                severity: 0,
+                condition: None},
+                {event: 'positive infant event',
+                severity: 0,
+                condition: None}
+            ],
+            mental:
+                [
+                {event: 'Listened to good music',
+                severity: 0,
+                condition: None}
+            ],
+            social:
+                [
+                {event: 'Played with ma-ma',
+                severity: 0,
+                condition: None}
+            ],
+        },
+        negative:{
+            physical:
+                [
+                {event: 'Ate a good meal',
+                severity: 0,
+                condition: None},
+                {event: 'positive infant event',
+                severity: 0,
+                condition: None}
+            ],
+            mental:
+                [
+                {event: 'Listened to good music',
+                severity: 0,
+                condition: None}
+            ],
+            social:
+                [
+                {event: 'Played with ma-ma',
+                severity: 0,
+                condition: None}
+            ],
+        }
+    },
+}
+
+let db_unique_milestone_events = {
+    infant: {
+        positive: [],
+        negative: []
+    },
+    toddler: {
+        positive: [],
+        negative: []
+    },
+    child: {
+        positive: [],
+        negative: []
+    },
+    young_adult: {
+        positive: [],
+        negative: []
+    },
+    adult: {
+        positive: [],
+        negative: []
+    }
 }
